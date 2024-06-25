db2d94afded3a61395a387b5c9e15790682cb646
zhiyi98 <yuanzhiyi98@gmail.com>
2024-06-25 23:55:20 +0800

    add authentication

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -17,23 +17,7 @@ export default function MailPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
+      <div className="flex-col flex">
         <Mail
           accounts={accounts}
           mails={mails}
--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -158,7 +158,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open: any) => {
+      onOpenChange: (open) => {
         if (!open) dismiss()
       },
     },
